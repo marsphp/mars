b0d6bfe7b45057b9c87a28fbd42dfe3d9e4ae8ae
Hassane DAO <dao.hassane@gmail.com>
2024-12-26 18:09:10 +0000

    feat: integrate dependency injection container and dotenv
    - Added support for Dotenv to manage environment variables
    - Set up a DI container with ReflectionContainer and ConfigServiceProvider
    - Updated app configuration and routing setup

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,9 +1,43 @@
 <?php
 
-use Mars\App;
+use Dotenv\Dotenv;
+use Laminas\Diactoros\Response;
+use Laminas\HttpHandlerRunner\Emitter\SapiEmitter;
+use League\Container\ReflectionContainer;
+use Mars\Framework\App;
+use Mars\Framework\Config\Config;
+use Mars\Framework\Container;
+use Mars\Framework\Providers\ConfigServiceProvider;
 
-require '../vendor/autoload.php';
+error_reporting(0);
 
-$app = new App();
+require_once __DIR__ . '/../vendor/autoload.php';
+
+$dotenv = Dotenv::createImmutable(__DIR__ . '/../');
+$dotenv->load();
+
+$container =  Container::getInstance();
+
+$container->delegate(new ReflectionContainer());
+
+$container->addServiceProvider(new ConfigServiceProvider());
+
+$config = $container->get(Config::class);
+
+foreach ($config->get('app.providers') as $provider) {
+    $container->addServiceProvider(new $provider);
+}
+
+$app = new App($container);
+
+$router = $container->get(\League\Route\Router::class);
+
+$app->getRouter()->get('/', function () {
+    $res = new Response();
+
+    $res->getBody()->write('Hello World!');
+
+    return $res;
+});
 
 $app->run();
