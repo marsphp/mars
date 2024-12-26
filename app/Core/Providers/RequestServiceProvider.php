feebb5e4c228078c1b99e39d9fc8f72a51224b9f
Hassane DAO <dao.hassane@gmail.com>
2024-12-26 18:09:30 +0000

    feat: add RequestServiceProvider for request handling
    - Implements boot and register methods
    - Registers shared Request instance using ServerRequestFactory

---
+++ app/Core/Providers/RequestServiceProvider.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Mars\Framework\Providers;
+
+use Laminas\Diactoros\Request;
+use Laminas\Diactoros\ServerRequestFactory;
+use League\Container\ServiceProvider\AbstractServiceProvider;
+use League\Container\ServiceProvider\BootableServiceProviderInterface;
+
+class RequestServiceProvider extends AbstractServiceProvider implements BootableServiceProviderInterface
+{
+
+    public function boot(): void
+    {
+        // TODO: Implement boot() method.
+    }
+
+    public function register(): void
+    {
+        $this->getContainer()->addShared(Request::class, function () {
+            return ServerRequestFactory::fromGlobals(
+                $_SERVER, $_GET, $_POST, $_COOKIE, $_FILES
+            );
+        });
+    }
+
+    public function provides(string $id): bool
+    {
+        $services = [
+            Request::class,
+        ];
+
+        return in_array($id, $services);
+    }
+}
