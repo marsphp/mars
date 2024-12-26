47e25c9efbf164d11d09b8460989ee36adc8951b
Hassane DAO <dao.hassane@gmail.com>
2024-12-26 18:09:24 +0000

    feat: add ConfigServiceProvider for configuration management
    - Implements ConfigServiceProvider with boot and register methods
    - Adds support for merging configuration files into the service container

---
+++ app/Core/Providers/ConfigServiceProvider.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Mars\Framework\Providers;
+
+use League\Container\ServiceProvider\AbstractServiceProvider;
+use League\Container\ServiceProvider\BootableServiceProviderInterface;
+use Mars\Framework\Config\Config;
+use Spatie\Ignition\Ignition;
+
+class ConfigServiceProvider extends AbstractServiceProvider implements BootableServiceProviderInterface
+{
+    public function boot(): void
+    {
+        // TODO: Implement boot() method.
+    }
+
+    public function register(): void
+    {
+        $this->getContainer()->addShared(Config::class, function () {
+            $config = new Config();
+
+            return $this->mergeConfigFromFiles($config);
+        });
+    }
+
+    public function provides(string $id): bool
+    {
+        $services = [
+            Config::class,
+        ];
+
+        return in_array($id, $services);
+    }
+
+    protected function mergeConfigFromFiles(Config $config): Config
+    {
+        $path = __DIR__ . '/../../../config';
+
+       foreach (array_diff(scandir($path), ['.', '..']) as $file) {
+           $config->merge([
+               explode('.', $file)[0] => require($path . '/' . $file),
+           ]);
+       }
+
+      return $config;
+    }
+}
