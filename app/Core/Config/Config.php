04d432880626c3d99d984b9a13ade1f356192379
Hassane DAO <dao.hassane@gmail.com>
2024-12-26 18:08:57 +0000

    feat: add Config class for managing application configuration
    - Implements merge method to merge configurations
    - Adds get method to retrieve configuration values

---
+++ app/Core/Config/Config.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Mars\Framework\Config;
+
+class Config
+{
+    protected array $config = [];
+
+    public function merge(array $config): static
+    {
+        $this->config = array_merge($this->config, $config);
+
+        return $this;
+    }
+
+    public function get(string $key, $default = null)
+    {
+        return dot($this->config)->get($key) ?? $default;
+    }
+}
