5170b223ec498203397e23a436ba7679f3dda202
Hassane DAO <dao.hassane@gmail.com>
2024-12-27 02:28:33 +0000

    Use Str::upper to return a fixed uppercase response

--- src/Interface/Http/Controllers/HomeController.php
+++ src/Interface/Http/Controllers/HomeController.php
@@ -4,6 +4,7 @@ namespace App\Interface\Http\Controllers;
 
 use Laminas\Diactoros\Request;
 use Laminas\Diactoros\Response;
+use Mars\Support\Facades\Str;
 use Psr\Http\Message\ServerRequestInterface;
 
 class HomeController
@@ -12,7 +13,7 @@ class HomeController
     {
         $response = new Response();
 
-        $response->getBody()->write($request->getQueryParams()['name']);
+        $response->getBody()->write(Str::upper('hello'));
 
         return $response;
     }
